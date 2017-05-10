231fda41394ce7f6b0f2526d90da98b19a146e00
PayAsian <PayAsian@yahoo.com>
2017-05-10 12:29:03 -0600

    making a new project

---
+++ src/controller/Controller.java
@@ -0,0 +1,30 @@
+package controller;
+
+import model.*;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class Controller 
+{
+	private Stack<Plates> Plates;
+	private Queue<Customer> Customer;
+	
+	public void Plates()
+	{
+		
+	}
+	
+	public void Customer()
+	{
+		Customer = new LinkedList<Customer>();
+	}
+	
+	public void start()
+	{
+		
+	}
+	
+	
+	
+}
